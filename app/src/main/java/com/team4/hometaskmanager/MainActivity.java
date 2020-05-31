b059ed44f8bc56caf4243657f868cc853371255f
Jaep Cuperus <jaepcuperus@hotmail.com>
2020-05-31 23:14:00 +0200

    Added material components, bottom navigation

--- app/src/main/java/com/team4/hometaskmanager/MainActivity.java
+++ app/src/main/java/com/team4/hometaskmanager/MainActivity.java
@@ -1,22 +1,42 @@
 package com.team4.hometaskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.team4.hometaskmanager.tasks.TasksListActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this.getApplicationContext();
+
+        BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.tasks_page_menu_item:
+                        startActivity(new Intent(context, TasksListActivity.class));
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void openTasksActivity(View view) {
-        startActivity(new Intent(this, TasksListActivity.class));
+        startActivity(new Intent(context, TasksListActivity.class));
     }
 }
