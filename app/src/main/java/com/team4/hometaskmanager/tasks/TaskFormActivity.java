45bf9d3f893557ee8502f1bc328c17245c7c0d2a
Jaep Cuperus <jaepcuperus@hotmail.com>
2020-06-02 16:25:14 +0200

    Task form data binding setup

--- app/src/main/java/com/team4/hometaskmanager/tasks/TaskFormActivity.java
+++ app/src/main/java/com/team4/hometaskmanager/tasks/TaskFormActivity.java
@@ -1,21 +1,28 @@
 package com.team4.hometaskmanager.tasks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import com.team4.hometaskmanager.R;
+import com.team4.hometaskmanager.databinding.ActivityTaskFormBinding;
 
 public class TaskFormActivity extends AppCompatActivity {
 
+    TaskViewModel taskViewModel;
     String[] groups = new String[] { "Home", "Work" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_form);
+
+        ActivityTaskFormBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_task_form);
+        taskViewModel = new TaskViewModel(Task.TASKS[0]);
+        binding.setTaskViewModel(taskViewModel);
 
         AutoCompleteTextView groupDropdown = findViewById(R.id.group_exposed_dropdown);
         groupDropdown.setAdapter(new ArrayAdapter<String>(getApplicationContext(), R.layout.dropdown_menu_popup_item, groups));
---
+++ app/src/main/java/com/team4/hometaskmanager/tasks/TaskViewModel.java
@@ -0,0 +1,39 @@
+package com.team4.hometaskmanager.tasks;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class TaskViewModel extends BaseObservable {
+    private Task task;
+
+    public TaskViewModel(Task task) {
+        this.task = task;
+    }
+
+    @Bindable
+    public Integer getGroupId() {
+        return task.groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        task.groupId = groupId;
+    }
+
+    @Bindable
+    public String getName() {
+        return task.name;
+    }
+
+    public void setName(String name) {
+        task.name = name;
+    }
+
+    @Bindable
+    public String getDescription() {
+        return task.description;
+    }
+
+    public void setDescription(String description) {
+        task.description = description;
+    }
+}
